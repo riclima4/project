f6155e73319efa26c77b7f295154d32bbe804926
GoncaloPeixoto <goncalopeixoto03@gmail.com>
2023-01-12 14:15:12 +0000

    server import
    server import

---
+++ Server/models/cars.js
@@ -0,0 +1,39 @@
+import Sequelize from "sequelize";
+import { dbInstance } from "../config/db.js";
+
+const CarsModel = dbInstance.define("cars", {
+  idCarro: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  nome: {
+    type: Sequelize.STRING(100),
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  marca: {
+    type: Sequelize.STRING(100),
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  matricula: {
+    type: Sequelize.STRING(6),
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  kilometragem: {
+    type: Sequelize.INTEGER(50),
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+});
+export { CarsModel };
