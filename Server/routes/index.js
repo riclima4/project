905f786dd562e7b81c2c786a42ee6bb31e94a3fb
GoncaloPeixoto <goncalopeixoto03@gmail.com>
2023-01-12 17:10:19 +0000

    .

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -18,7 +18,20 @@ import {
   updateIntervencao,
 } from "../controllers/intervencao.js";
 const routes = Router();
+
 routes.get("/users", getAllUsers);
+routes.get("/user/:idUser", getUsersById);
+routes.get("/newCar", newUser);
+routes.get("/updateCar", updateUser);
+
 routes.get("/cars", getAllcars);
+routes.get("/cars", getAllcars);
+routes.get("/cars", getAllcars);
+routes.get("/cars", getAllcars);
+
 routes.get("/intervencao", getAllIntervencao);
+routes.get("/intervencao", getAllIntervencao);
+routes.get("/intervencao", getAllIntervencao);
+routes.get("/intervencao", getAllIntervencao);
+
 export { routes };
