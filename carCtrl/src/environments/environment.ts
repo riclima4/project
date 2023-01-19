d1f61520f313c68e9133585ead75c4e451de5289
Ricardo Lima <ric.lima1@hotmail.com>
2023-01-19 01:02:46 +0000

    Update environment.ts

--- carCtrl/src/environments/environment.ts
+++ carCtrl/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   api_url: 'http://localhost:4242',
   session: {
     TOKEN: 'token',
